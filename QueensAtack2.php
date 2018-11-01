78f182e4dd764c418d77d2f77596400d44a30849
Yuri <firstbitrix@ya.ru>
2018-11-01 21:38:11 +1000

    Улучшен внешний вид кода (изначально делался по-быстрому для решения задачи), добавлен комментарий для работодателя. Код проверен на работоспособность.

--- QueensAtack2.php
+++ QueensAtack2.php
@@ -1,59 +1,64 @@
 <?php
+/* Это решение задачи, описанной здесь: https://www.hackerrank.com/challenges/queens-attack-2/problem
+Это пока одна из самых сложных, решённых мной на этом сайте. 
+Используется процедурный стиль, однако я привёл его в более красивую форму
+Для проверки можно встааить весь этот код вместо фнкции function queensAttack($n, $k, $r_q, $c_q, $obstacles) {}
+И проверить его работу
+ */
+ 
+//
 $arDim = array(
-    array(1, 0),
-    array(1, 1),
-    array(0, 1),
-    array(-1, 1),
-    array(-1, 0),
-    array(-1, -1),
-    array(0, -1),
-    array(1, -1),
+    array(1, 0), array(1, 1), array(0, 1), 
+	array(-1, 1), array(-1, 0),
+	array(-1, -1), array(0, -1), array(1, -1)
 );
 $arDist = array();
     
 // Complete the queensAttack function below.
 function queensAttack($n, $k, $r_q, $c_q, $obstacles) {
     
-    //1. board Distance
+    //1. Board free distance calculation
     global $arDim;
     $qPoint = array($c_q, $r_q);
+	
     foreach($arDim as $dim){
         foreach($dim as $key => $direction){
+			
             if ($direction > 0) $distance = $n - $qPoint[$key];
             elseif ($direction < 0) $distance = $qPoint[$key] - 1;
             else $distance = 0;
             $arCurDist[$key] = $distance;
+			
         }
-        //var_dump($dim, $arCurDist);
         if (hasEmpty($dim)) $distance = max($arCurDist); //not null
         else $distance = min($arCurDist);
         $arDist[$dim[0]][$dim[1]] = $distance;
     }
     
-    //var_dump($c_q, $r_q);
-    //2. Obstacle distance
+    //2. Obstacle distance calculating
     
-    foreach($obstacles as $ob){
-        $oc = $ob[1];
-        $or = $ob[0];
-        $c = abs($c_q - $oc);
-        $r = abs($r_q - $or);
-        //var_dump($ob, $c, $r);
+    foreach($obstacles as $obstacle){
+        $obColumn = $obstacle[1];
+        $obRow = $obstacle[0];
+        $column = abs($c_q - $obColumn);
+        $row = abs($r_q - $obRow);
+        //var_dump($ob, $column, $row);
         //intersection
-        if ($c == $r || $c == 0 || $r == 0){
-            $obDimCol = -($c_q <=> $oc);
-            $obDimRow = -($r_q <=> $or);
+        if ($column == $row || $column == 0 || $row == 0){
+            $obDimCol = -($c_q <=> $obColumn);
+            $obDimRow = -($r_q <=> $obRow);
             //var_dump($obDimCol, $obDimRow);
-            $arCurDist = array($c, $r);
-            if ($c == $r) $dist = $c;
+            $arCurDist = array($column, $row);
+            
+            if ($column == $row) $dist = $column;
             else $dist = max($arCurDist);
-            $arDist[$obDimCol][$obDimRow] = $dist - 1;
-            //var_dump($arDist);
+            $dist -= 1;
+            
+            if ($arDist[$obDimCol][$obDimRow] > $dist) $arDist[$obDimCol][$obDimRow] = $dist;
         }
-        //var_dump($ob);
     }
     
-    //3. calculating
+    //3. Calculating all dimensions
     $amount = calculate($arDist);
     echo $amount.PHP_EOL;
     return $amount;
@@ -70,35 +75,3 @@ function calculate($arDist){
         $amount += array_sum($column);
     return $amount;    
 }
-
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
-
-$stdin = fopen("php://stdin", "r");
-
-fscanf($stdin, "%[^\n]", $nk_temp);
-$nk = explode(' ', $nk_temp);
-
-$n = intval($nk[0]);
-
-$k = intval($nk[1]);
-
-fscanf($stdin, "%[^\n]", $r_qC_q_temp);
-$r_qC_q = explode(' ', $r_qC_q_temp);
-
-$r_q = intval($r_qC_q[0]);
-
-$c_q = intval($r_qC_q[1]);
-
-$obstacles = array();
-
-for ($i = 0; $i < $k; $i++) {
-    fscanf($stdin, "%[^\n]", $obstacles_temp);
-    $obstacles[] = array_map('intval', preg_split('/ /', $obstacles_temp, -1, PREG_SPLIT_NO_EMPTY));
-}
-
-$result = queensAttack($n, $k, $r_q, $c_q, $obstacles);
-
-fwrite($fptr, $result . "\n");
-
-fclose($stdin);
-fclose($fptr);
